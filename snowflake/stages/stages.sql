1a360133e0ab9c9fdce6c27d487c9c659dd72fe2
Anasbendahmane <bendahmenanas4@gmail.com>
2026-03-09 22:00:10 +0100

    feat: add external stages for S3

---
+++ snowflake/stages/stages.sql
@@ -0,0 +1,23 @@
+---stage for TRANSACTIONS
+create or replace stage banking_db.RAW.transaction_stage 
+file_format= (type=csv,field_delimiter=',',skip_header=1)
+URL='s3://banking-insights-bucket/transactions/'
+storage_integration = S3_banking_storage ;
+
+
+----stage for users
+create or replace stage banking_db.RAW.users_stage 
+file_format= (type=csv,field_delimiter=',',skip_header=1)
+URL='s3://banking-insights-bucket/users/'
+storage_integration = S3_banking_storage ;
+
+
+--- stage for cards
+
+create or replace stage banking_db.RAW.cards_stage 
+file_format= (type=csv,field_delimiter=',',skip_header=1)
+URL='s3://banking-insights-bucket/cards/'
+storage_integration = S3_banking_storage ;
+
+
+LIST @banking_db.raw.transaction_stage;
