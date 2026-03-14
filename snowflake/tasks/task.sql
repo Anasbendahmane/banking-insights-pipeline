d0344e47441442c628648b880ebd2ec0847d9a87
a2bendah <bendahmenanas4@gmail.com>
2026-03-14 06:56:13 +0100

    feat: add tasks for RAW tables loading

--- snowflake/tasks/task.sql
+++ snowflake/tasks/task.sql
@@ -3,18 +3,28 @@ use warehouse banking_warehouse;
 use database banking_db;
 use schema RAW;
 
-CREATE OR REPLACE TASK insert_user
-warehouse ='banking_warehouse'
+CREATE OR REPLACE TASK task_user
+warehouse = banking_warehouse
 schedule = '60 MINUTES'
 AS 
     call BANKING_DB.RAW.PROC_USER();
 
 
-EXECUTE TASK insert_user ;
-
-
-CREATE OR REPLACE TASK insert_cards
-warehouse ='banking_warehouse'
-AFTER insert_user
+CREATE OR REPLACE TASK task_card
+warehouse = banking_warehouse
+SCHEDULE = '60 MINUTES'
 AS 
-    call BANKING_DB.RAW.PROC_card();
+    call BANKING_DB.RAW.PROC_CARD();
+
+
+
+CREATE OR REPLACE TASK task_transactions
+warehouse = banking_warehouse
+SCHEDULE = '60 MINUTES'
+AS
+    call BANKING_DB.RAW.PROC_TRANSACTIONS();
+
+
+EXECUTE TASK task_user ;
+EXECUTE TASK task_card;
+EXECUTE TASK task_transactions;
